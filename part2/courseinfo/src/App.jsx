32c2b7e00e9087c94992d9b7c0289a8e3e90b38e
lumikil <kilpelainenlumi@gmail.com>
2024-12-17 10:19:26 +0200

    Initialize React project with Vite setup and implement course information display

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,94 @@
+const Header = (props) => {
+  return <h2>{props.course}</h2>
+}
+
+const Part = (props) => {
+  return <p>{props.part.name} {props.part.exercises}</p>
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map(part => 
+        <Part key={part.id} part={part} />
+      )}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  const total = props.part.reduce((sum, part) => sum + part.exercises, 0);
+
+  return (
+    <p>
+      <b>total of {total} exercises</b>
+    </p>
+  );
+};
+
+const Course = (props) => {
+  return (
+    <div>
+      <Header course={props.course.name} />
+      <Content parts={props.course.parts} />
+      <Total part={props.course.parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+      <Course course={courses[0]} />
+      <Course course={courses[1]} />
+    </div>
+  )
+}
+
+export default App
