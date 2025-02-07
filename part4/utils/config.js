fa7d6e8165c781f3296cbd305800aea7f3981428
lumikil <kilpelainenlumi@gmail.com>
2025-02-07 12:35:33 +0200

    part4

---
+++ part4/utils/config.js
@@ -0,0 +1,14 @@
+/**
+ * Handling of environment variables
+ * Why? To avoid hardcoding of sensitive information
+ */
+require('dotenv').config()
+
+const PORT = process.env.PORT
+const MONGODB_URI = process.env.MONGODB_URI
+
+module.exports = {
+    MONGODB_URI,
+    PORT
+}
+
---
+++ part4/utils/list_helper.test.js
@@ -0,0 +1,3 @@
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
+const listHelper = require('../utils/list_helper')
